fe61106f87a4db4522dc0a4c17e4fa09cbeb9cdc
yashelter <yashelter@yandex.ru>
2026-03-30 10:47:49 +0300

    fixed Tkey restriction, new Tests, better Random

--- TreeDataStructures/Implementations/BST/BinarySearchTree.cs
+++ TreeDataStructures/Implementations/BST/BinarySearchTree.cs
@@ -3,7 +3,6 @@
 namespace TreeDataStructures.Implementations.BST;
 
 public class BinarySearchTree<TKey, TValue> : BinarySearchTreeBase<TKey, TValue, BstNode<TKey, TValue>>
-    where TKey : IComparable<TKey>
 {
     protected override BstNode<TKey, TValue> CreateNode(TKey key, TValue value)
     {
