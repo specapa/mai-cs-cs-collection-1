d8ec62edbf53e6376edbf882cae4bdec3a820f79
zeotq_homepc_wn11 <specapa@yandex.ru>
2026-04-04 11:37:57 +0300

    Реализация Splay

--- TreeDataStructures/Implementations/Splay/SplayTree.cs
+++ TreeDataStructures/Implementations/Splay/SplayTree.cs
@@ -11,17 +11,88 @@ public class SplayTree<TKey, TValue> : BinarySearchTree<TKey, TValue>
     
     protected override void OnNodeAdded(BstNode<TKey, TValue> newNode)
     {
-        throw new NotImplementedException();
+        // Splay the newly added node to the root
+        Splay(newNode);
     }
     
     protected override void OnNodeRemoved(BstNode<TKey, TValue>? parent, BstNode<TKey, TValue>? child)
     {
-        throw new NotImplementedException();
+        // After removal, splay the parent if present, otherwise splay the child
+        if (parent != null) Splay(parent);
+        else if (child != null) Splay(child);
     }
     
     public override bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
     {
-        throw new NotImplementedException();
+        var node = typeof(BinarySearchTree<TKey, TValue>)
+            .GetMethod("FindNode", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+            ?.Invoke(this, new object[] { key }) as BstNode<TKey, TValue>;
+
+        if (node != null)
+        {
+            Splay(node);
+            value = node.Value;
+            return true;
+        }
+
+        value = default;
+        return false;
+    }
+
+    public override bool ContainsKey(TKey key)
+    {
+        var node = typeof(BinarySearchTree<TKey, TValue>)
+            .GetMethod("FindNode", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+            ?.Invoke(this, new object[] { key }) as BstNode<TKey, TValue>;
+
+        if (node != null)
+        {
+            Splay(node);
+            return true;
+        }
+        return false;
+    }
+
+    private void Splay(BstNode<TKey, TValue>? x)
+    {
+        while (x != null && x.Parent != null)
+        {
+            var p = x.Parent;
+            var g = p.Parent;
+
+            if (g == null)
+            {
+                // Zig
+                if (x.IsLeftChild)
+                    RotateRight(p);
+                else
+                    RotateLeft(p);
+            }
+            else if (x.IsLeftChild && p.IsLeftChild)
+            {
+                // Zig-Zig
+                RotateRight(g!);
+                RotateRight(p);
+            }
+            else if (x.IsRightChild && p.IsRightChild)
+            {
+                // Zig-Zig
+                RotateLeft(g!);
+                RotateLeft(p);
+            }
+            else if (x.IsRightChild && p.IsLeftChild)
+            {
+                // Zig-Zag
+                RotateLeft(p);
+                RotateRight(g!);
+            }
+            else if (x.IsLeftChild && p.IsRightChild)
+            {
+                // Zig-Zag
+                RotateRight(p);
+                RotateLeft(g!);
+            }
+        }
     }
     
 }
